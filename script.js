7401ebbc3943f690435b29fa5f71df95dfbc84f0
outlowe <procrackher@gmail.com>
2024-09-13 00:19:33 +0300

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
+// Basit bir JavaScript işlevi ekleyebilirsiniz
 document.addEventListener('DOMContentLoaded', () => {
-    const url = 'https://meowfacts.herokuapp.com/';
-
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('facts').innerHTML = `
-                <p>${data.fact}</p>
-            `;
-        })
-        .catch(error => console.error('API çağrısında bir hata oluştu:', error));
+    console.log('Sayfa yüklendi.');
 });
